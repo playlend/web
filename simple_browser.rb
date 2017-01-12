0330386bbd7eaf73ec6260ffcf2d57b47d474159
Taras <tarasyakovchuk@gmail.com>
2017-01-12 11:25:23 -0800

    working on ability to send and accept post requests

--- simple_browser.rb
+++ simple_browser.rb
@@ -1,11 +1,27 @@
 require 'socket'
+require 'json'
  
 host = 'localhost'     # The web server
 port = 2000                          # Default HTTP port
 path = "/index.html"                 # The file we want 
 
+print "Enter 1 for GET request, enter 2 for POST request: "
+answer = gets.chomp
+answer = answer.to_i
 # This is the HTTP request we send to fetch a file
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+if answer == 1
+	request = "GET #{path} HTTP/1.0\r\n\r\n"
+elsif answer == 2
+	data = Hash.new()
+	print "Please, enter your name: "
+	data["name"] = gets.chomp
+	print "Please, enter your email: "
+	data["email"] = gets.chomp
+	request = data.to_json
+end	
+
+
+
 
 socket = TCPSocket.open(host,port)  # Connect to server
 socket.print(request)               # Send request
